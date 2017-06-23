aa986b8d3b18c04dc049324b893de0f1c1faebb6
Yakprod <nosanosaasi@gmail.com>
2017-06-23 16:32:26 +0300

    header-slider

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
 	// Custom JS
-
+	$('.main').owlCarousel({
+		loop: true,
+		items: 1,
+		smartSpeed: 700,
+		margin: 0,
+		nav: true,
+		navText: ['', ''],
+		// navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+	});
 });
