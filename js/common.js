97a1a7e6f88f81eb692ed9e4305346c55df3f5f8
Yakprod <nosanosaasi@gmail.com>
2017-06-24 03:25:08 +0300

    responsive

--- js/common.js
+++ js/common.js
@@ -10,4 +10,23 @@ $(function() {
 		navText: ['', ''],
 		// navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 	});
+
+	$(document).ready(function(){
+		var link = $('.menu-link');
+		var link_active = $('.menu-link_active');
+		var menu = $('.menu');
+
+		link.click(function(){
+			link.toggleClass('menu-link_active');
+			menu.toggleClass('menu_active');
+		});
+		link_active.click(function(){
+			link.removeClass('menu-link_active');
+		});
+	});
+
+	$(".mmenu a").mPageScroll2id({
+		offset: 0
+	})
+
 });
